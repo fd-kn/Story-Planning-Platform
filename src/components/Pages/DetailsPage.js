227556da3540cda7592126b709e39954687eb0fb
Farid Khan <farid41658@gmail.com>
2024-11-03 20:05:22 +0000

    removed even more unused stuff

--- src/components/Pages/DetailsPage.js
+++ src/components/Pages/DetailsPage.js
@@ -174,10 +174,6 @@ const DetailsPage = () => {
   }
 
   const renderFields = () => {
-    const commonProps = {
-      onChange: handleChange,
-      className: "w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none focus:border-blue-500"
-    };
 
 
     switch (category) {
